a9b6119949bd94c18906dc3b3b218499f079fc89
DeepKondal <abideep.singh97@gmail.com>
2024-02-23 03:35:29 -0500

    Initial commit

---
+++ 02-backend/Spring-boot-ecommerce/Spring-boot-ecommerce/src/main/java/com/Unicorn/Springbootecommerce/config/MyDataRestConfig.java
@@ -0,0 +1,34 @@
+package com.Unicorn.Springbootecommerce.config;
+
+import com.Unicorn.Springbootecommerce.entity.Product;
+import com.Unicorn.Springbootecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors);
+        HttpMethod[] theUnsupportedAction = {HttpMethod.PUT,HttpMethod.POST,HttpMethod.DELETE};
+
+        // disbale these 3 menthods for Products
+
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedAction))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedAction));
+
+        // disbale these 3 menthods for ProductCategory
+
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedAction))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedAction));
+
+    }
+}
